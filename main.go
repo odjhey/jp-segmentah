056e30e27f364a249d9287bc92a91062b5beba44
odjhey <odjhey@gmail.com>
2023-10-26 16:35:20 +0800

    feat: build for js-colorizah

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/go-ego/gse"
 )
@@ -20,11 +21,13 @@ func main() {
 
 	var seg gse.Segmenter
 	// seg.LoadDict("../data/dict/dictionary.txt")
-	err := seg.LoadDict("jp")
-	fmt.Println("load dictionary error: ", err)
+	// err := seg.LoadDict("jp")
+	seg.LoadDict("jp")
+	// fmt.Println("load dictionary error: ", err)
 
-	err = seg.LoadDict("./custom-dict.txt")
-	fmt.Println("load dictionary error: ", err)
+	// err = seg.LoadDict("./custom-dict.txt")
+	seg.LoadDict("./custom-dict.txt")
+	// fmt.Println("load dictionary error: ", err)
 
 	// fmt.Println(seg.Cut(*text, true))
 	// fmt.Println(seg.CutAll(*text))
@@ -37,19 +40,33 @@ func main() {
 	// fmt.Println(gse.ToString(segments, true))
 
 	// read from filesystem 01.srt
-	text2, err := ioutil.ReadFile("01.srt")
-	fmt.Println("file read err: ", err)
+	// text2, err := ioutil.ReadFile("01.srt")
+	// text2, err := ioutil.ReadFile("kokoro1.txt")
+	// fmt.Println("file read err: ", err)
 
 	// loop at filecontents per line
 	// split bytes string into lines at newline
-	texts := bytes.Split(text2, []byte("\n"))
-	for _, text := range texts {
-		fmt.Println(">---", string(text))
+	// texts := bytes.Split(text2, []byte("\n"))
+
+	// read texts from stdin
+	// var texts []byte
+	// fmt.Scan(&texts)
+	// read all texts from stdin
+	texts, _ := ioutil.ReadAll(os.Stdin)
+	// fmt.Println("std read", err)
+
+	// stdin to string array
+	texts2 := strings.Split(string(texts), "\n")
+	// fmt.Println(texts2)
+
+	for _, text := range texts2 {
+		text := []byte(text)
 		segs := seg.Segment(text)
 		// print segs array
 		for _, seg := range segs {
 			// fmt.Println(seg.Token().Pos(), seg.Token().Text(), seg.Token().Freq())
-			fmt.Println("<---", seg.Token().Text())
+			// fmt.Println("<---", seg.Token().Text())
+			fmt.Println(seg.Token().Pos(), "\t", seg.Token().Text())
 		}
 	}
 
